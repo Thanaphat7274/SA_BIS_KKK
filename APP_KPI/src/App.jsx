08c1ef3b77a60a5fe790b544ad133b01736dab30
Thanaphat245 <bellplaza555@gmail.com>
2025-10-04 20:25:12 +0700

    Merge changes from sildebar into main

--- APP_KPI/src/App.jsx
+++ APP_KPI/src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Sidebar from './components/Sildebar'
+import KPIPage from './pages/KPI_page'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Content from './components/Content'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +12,14 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Sidebar />} />
+          <Route path="/" element={<KPIPage />}>
+            <Route index element={<Content />} />
+            <Route path="dashboard" element={<Content />} />
+            <Route path="evaluation" element={<Content />} />
+            <Route path="employees" element={<Content />} />
+            <Route path="attendance" element={<Content />} />
+            <Route path="reports" element={<Content />} />
+          </Route>
           <Route path="/about" element={<h1>About Page</h1>} />
         </Routes>
       </div>
