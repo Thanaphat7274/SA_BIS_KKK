65a212d185511f317b2635da443e89aa18a9cb32
Thanaphat7274 <sukchuen_t@su.ac.th>
2025-10-04 23:37:45 +0700

    new

---
+++ kpi_app/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='text-3xl font-bold underline text-blue-600 rounded-lg text-center'> dasdadsadas</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
